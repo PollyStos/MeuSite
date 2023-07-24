581727e33a56041e44539eca6e0253b89c02c267
polly_stos <polly_stos@yahoo.com.br>
2023-07-24 08:09:42 -0300

    Nova versão otimisada com PHP

---
+++ MeuSitePHP/php/blog.php
@@ -0,0 +1,42 @@
+<?php require_once "header.php" ?>
+
+<body>
+    <?php require_once "menu.php" ?>
+    <section class="blog">
+    <section class="txtPrinc">
+            <h1>Meu título</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aspernatur sit unde incidunt labore animi omnis recusandae minima voluptatibus accusantium fugit repellat, nihil consectetur nulla. Minima vero adipisci laborum quasi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum odio tempore, debitis facilis autem iure repudiandae at reprehenderit mollitia non vero laudantium a voluptatum soluta, quod consequuntur aspernatur harum eligendi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus accusantium quos placeat veniam eligendi quisquam vero reiciendis dolorem ea ipsa. Nesciunt maxime saepe labore sed ea neque asperiores provident velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quo explicabo saepe sunt blanditiis, nobis impedit nulla facere numquam minima aliquam voluptatem unde sed dolor quod eaque eligendi veritatis fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quaerat voluptatum ullam quo quia nulla, minus aliquid asperiores iusto iure aut nam vero ad deleniti velit quidem. Totam, harum quos! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, aliquid? Quibusdam, debitis mollitia quia quisquam delectus quasi error sint explicabo aspernatur vel vitae repudiandae totam recusandae molestias ullam enim. Corrupti.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, asperiores nisi? Delectus nesciunt sint suscipit aliquam autem, aut explicabo atque, a laborum doloribus molestiae vero architecto corrupti adipisci saepe quaerat.
+            </p>
+            <img src="../imagens/983717626056.jpg" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aspernatur sit unde incidunt labore animi omnis recusandae minima voluptatibus accusantium fugit repellat, nihil consectetur nulla. Minima vero adipisci laborum quasi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum odio tempore, debitis facilis autem iure repudiandae at reprehenderit mollitia non vero laudantium a voluptatum soluta, quod consequuntur aspernatur harum eligendi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus accusantium quos placeat veniam eligendi quisquam vero reiciendis dolorem ea ipsa. Nesciunt maxime saepe labore sed ea neque asperiores provident velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quo explicabo saepe sunt blanditiis, nobis impedit nulla facere numquam minima aliquam voluptatem unde sed dolor quod eaque eligendi veritatis fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quaerat voluptatum ullam quo quia nulla, minus aliquid asperiores iusto iure aut nam vero ad deleniti velit quidem. Totam, harum quos! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, aliquid? Quibusdam, debitis mollitia quia quisquam delectus quasi error sint explicabo aspernatur vel vitae repudiandae totam recusandae molestias ullam enim. Corrupti.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, asperiores nisi? Delectus nesciunt sint suscipit aliquam autem, aut explicabo atque, a laborum doloribus molestiae vero architecto corrupti adipisci saepe quaerat.
+            </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aspernatur sit unde incidunt labore animi omnis recusandae minima voluptatibus accusantium fugit repellat, nihil consectetur nulla. Minima vero adipisci laborum quasi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum odio tempore, debitis facilis autem iure repudiandae at reprehenderit mollitia non vero laudantium a voluptatum soluta, quod consequuntur aspernatur harum eligendi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus accusantium quos placeat veniam eligendi quisquam vero reiciendis dolorem ea ipsa. Nesciunt maxime saepe labore sed ea neque asperiores provident velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quo explicabo saepe sunt blanditiis, nobis impedit nulla facere numquam minima aliquam voluptatem unde sed dolor quod eaque eligendi veritatis fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quaerat voluptatum ullam quo quia nulla, minus aliquid asperiores iusto iure aut nam vero ad deleniti velit quidem. Totam, harum quos! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, aliquid? Quibusdam, debitis mollitia quia quisquam delectus quasi error sint explicabo aspernatur vel vitae repudiandae totam recusandae molestias ullam enim. Corrupti.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, asperiores nisi? Delectus nesciunt sint suscipit aliquam autem, aut explicabo atque, a laborum doloribus molestiae vero architecto corrupti adipisci saepe quaerat.
+            </p>
+        </section>
+        <section class="menuVert">
+            <ul>2023
+                <li>Janeiro</li>
+                <li>Fevereiro</li>
+                <li>Março</li>
+                <li>Abril</li>
+                <li>Maio</li>
+                <li>Junho</li>
+                <li>Julho</li>
+                <li>Agosto</li>
+                <li>Setembro</li>
+                <li>Outobro</li>
+                <li>Novembro</li>
+                <li>Dezembro</li>
+            </ul>
+        </section>
+    </section>
+
+    </body>
+<footer>
+ <?php require_once"footer.php"?>
+</footer>
+
+</html>
---
+++ MeuSitePHP/php/menu.php
@@ -0,0 +1,10 @@
+<nav id="idmenu" class="menu">
+        <ul>
+            <li><a href="index.php">Início</a></li>
+            <li><a href="aprender.php">Aprender</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="projetos.php">Projetos</a></li>
+            <li><a href="jogos.php">Jogos Online</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
+        </ul>
+    </nav>
