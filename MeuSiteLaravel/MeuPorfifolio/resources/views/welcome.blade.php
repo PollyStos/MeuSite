84a013c500c43e65634234ba84031c3b183c9ec2
polly_stos <polly_stos@yahoo.com.br>
2023-09-19 21:58:14 -0300

    Nov modelo de site Portifólio

---
+++ MeuSiteLaravel/MeuPorfifolio/resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Home</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.googleapis.com/css2?family=Aldrich&family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;900&display=swap" rel="stylesheet">
+
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+    
+    <div class="bg-menu p-3 m-4">
+        <div class="d-flex flex-column align-items-center" >
+            <h1>Portifólio</h1>
+            <h2>Polyana Santos Silva</h2>
+        </div>
+        <div class="p-3 d-flex flex-column align-items-center" >
+            <button class="bt-menu border-0" type="button">
+                <h3 class="" ><strong>projetos</strong></h3>
+            </button>
+            <button class="bt-menu border-0" type="button">
+                <h3 class="" ><strong>Jogos Online</strong></h3>
+            </button>
+            <button class="bt-menu border-0" type="button">
+                <h3 class="" ><strong>Blog</strong></h3>
+            </button>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
